f6307848b932e68c2568c7e39e9df9dd982346ae
Melnyk Dmytro <ekut2104@gmail.com>
2019-01-09 17:35:21 +0200

    Add models

--- gameshop/settings.py
+++ gameshop/settings.py
@@ -76,13 +76,13 @@ WSGI_APPLICATION = 'gameshop.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
-DATABASES['default'] = dj_database_url.config()
+# DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+class Player(models.Model):
+    def __str__(self):
+        return self.user.username
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Developer(models.Model):
+    def __str__(self):
+        return self.user.username
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 # Create your models here.
 class Game(models.Model):
