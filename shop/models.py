e4deca6c913987385c97f9999db4debd871b51bd
Melnyk Dmytro <ekut2104@gmail.com>
2019-01-09 18:33:33 +0200

    Add transaction

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class Player(models.Model):
@@ -15,7 +16,17 @@ class Developer(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+
 # Create your models here.
 class Game(models.Model):
     title = models.CharField(max_length=30, null=False, blank=False, unique=False)
     price = models.FloatField(null=False, blank=False, unique=False)
+    url = models.URLField(max_length=300, null=False, blank=False, unique=True)
+    developer = models.ForeignKey(Developer, on_delete=models.CASCADE)
+
+
+class Transaction(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    paid_amount = models.FloatField()
+    timestamp = models.DateField(default=timezone.now)
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-from .models import Game
+from django.contrib.auth.models import User, Group
+from shop.models import Game, Developer, Player
 
 
 # Create your views here.
@@ -31,5 +32,48 @@ def login_user(request):
     pass
 
 
+def home(request):
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect("shop:index")
+        games = Game.objects.all()
+        return render(request, "shop/home.html", {"games": games})
+    else:
+        return HttpResponse(status=500)
+
+
 def create(request):
-    pass
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        developer = False
+        try:
+            if request.POST['developer']:
+                developer = True
+        except KeyError:
+            developer = False
+        if username is not None and email is not None and password is not None:
+            if not username or not email or not password:
+                return render(request, "shop/signup.html", {"error": "Please fill in all required fields"})
+            if User.objects.filter(username=username).exists():
+                return render(request, "shop/signup.html", {"error": "Username already exists"})
+            elif User.objects.filter(email=email).exists():
+                return render(request, "shop/signup.html", {"error": "Email already exists"})
+            user = User.objects.create_user(username, email, password)
+            if developer:
+                if Group.objects.filter(name="developers").exists():
+                    dev_group = Group.objects.get(name="developers")
+                else:
+                    Group.objects.create(name='developers').save()
+                    dev_group = Group.objects.get(name='developers')
+                dev_group.user_set.add(user)
+                Developer.objects.create(user=user).save()
+            else:
+                Player.objects.create(user=user).save()
+            user.save()
+            login(request, user, backend="django.contrib.auth.backends.ModelBackend")
+        return redirect("shop:index")
+
+    else:
+        return redirect("shop:signup")
