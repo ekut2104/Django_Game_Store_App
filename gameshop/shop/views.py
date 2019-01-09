39c60fb35afe1860ececbbed8889e3dd96a023d7
Melnyk Dmytro <ekut2104@gmail.com>
2019-01-09 12:52:45 +0200

    add shopapps to settings_installad, add migrations, add models, basic views, and urls

--- gameshop/shop/views.py
+++ gameshop/shop/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Game
+
 
 # Create your views here.
+def index(request):
+    if request.method == "GET":
+        return HttpResponse("Hello World!")
+
+
+def get_games(request):
+    if request.method == "GET":
+        game = Game.objects.all()[0]
+        return HttpResponse(str(game.title) + " $" + str(game.price))
+
+
+def get_html(request):
+    if request.method == "GET":
+        return render(request, 'shop/index.html')
